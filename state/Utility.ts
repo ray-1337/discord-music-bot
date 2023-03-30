750bf72dd77892906accfe92a84ad8148d857f62
ray-1337 <33544674+ray-1337@users.noreply.github.com>
2023-03-30 10:25:56 +0200

    added chunk list on utility

--- state/Utility.ts
+++ state/Utility.ts
@@ -9,3 +9,14 @@ export function pascalToCamel(str: string) {
 export function truncateString(str: string, num: number) {
   return str.length > num ? str.slice(0, num) + "..." : str;
 };
+
+// chunk array
+export function chunk(array: any[], chunkSize: number) {
+  const temp: any[] = [];
+
+  for (let i = 0; i < array.length; i += chunkSize) {
+    temp.push(array.slice(i, i + chunkSize));
+  };
+
+  return temp;
+};
