1f88066b239a84b172800b14b504550b26559c69
ray-1337 <33544674+ray-1337@users.noreply.github.com>
2023-03-31 00:17:38 +0200

    added slash command typings

---
+++ typings/Command.d.ts
@@ -0,0 +1,23 @@
+// for commandCache
+interface CommandSmallDetails {
+  path: string;
+  raw: string;
+}
+
+// command handler section
+interface CommandInfo {
+  name?: string;
+  description?: string;
+}
+
+interface CommandInterface {
+  config: CommandConfig;
+  info: CommandInfo;
+  run: (client: import("oceanic.js").Client, interaction: import("oceanic.js").CommandInteraction, ...args) => Promise<void>;
+  args?: import("oceanic.js").ApplicationCommandOptionsWithValue[];
+}
+
+interface CommandConfig {
+  cooldown?: number;
+  permissions?: (keyof typeof import("oceanic.js")["Constants"]["Permissions"])[];
+}
