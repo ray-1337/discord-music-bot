89e2d6effc7249da96a5da9eeb170389fbeb5e44
ray-1337 <33544674+ray-1337@users.noreply.github.com>
2023-03-30 11:39:19 +0200

    added voiceChannelLeave event to control empty vc

---
+++ events/voiceChannelLeave.ts
@@ -0,0 +1,17 @@
+import { Client, Uncached, VoiceChannel, StageChannel, Member } from "oceanic.js";
+import Music from "../state/Music";
+
+export default async (client: Client, _: Member, unprocessedChannel: Uncached | VoiceChannel | StageChannel) => {
+  try {
+    if (unprocessedChannel?.id) {
+      const channel = client?.getChannel<VoiceChannel | StageChannel>(unprocessedChannel.id) || await client.rest.channels.get<VoiceChannel | StageChannel>(unprocessedChannel.id);
+      if (!channel) return;
+  
+      if (channel.voiceMembers.filter(val => !val.bot).length <= 0) {
+        return Music.stop(channel.guildID);
+      };
+    };
+  } catch (error) {
+    return console.error(error);
+  };
+};
