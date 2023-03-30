dc29197f65f5f8f8aa50c0b4cc03b79ca7a77785
ray-1337 <33544674+ray-1337@users.noreply.github.com>
2023-03-31 00:17:03 +0200

    added slash command manager

---
+++ state/SlashCommand.ts
@@ -0,0 +1,74 @@
+import { Client, Constants, CreateChatInputApplicationCommandOptions, CreateApplicationCommandOptions } from "oceanic.js";
+import path from "node:path";
+import { readdir, lstat } from "node:fs/promises";
+import { pascalToCamel, truncateString } from "./Utility";
+
+export const commandCache: GuardedMap<string, CommandSmallDetails> = new Map();
+
+const fileSpecificRegex = /\.(t|j)s+$/gi;
+
+export default async function (client: Client) {
+  try {
+    // for analytics
+    const firstTime = performance.now();
+    const finaleCommand: (CreateChatInputApplicationCommandOptions & CreateApplicationCommandOptions)[] = [];
+    const cmdPath = path.join(__dirname, "..", "commands");
+    const cmdFolder = await readdir(cmdPath);
+
+    // im not interested in sub command in this, so ..
+    // but seriously you can modify this by yourself
+    for await (const category of cmdFolder) {
+      let commandPath = path.join(cmdPath, category);
+      let firstCommandType = await lstat(commandPath);
+
+      if (firstCommandType.isFile() && category.match(fileSpecificRegex)) {
+        const command = require(commandPath) as CommandInterface;
+
+        // ignore nothing
+        if (!command?.info && !command?.run && !command?.args) {
+          continue;
+        };
+
+        const cmdName = pascalToCamel((command.info?.name || category).toLowerCase().replace(fileSpecificRegex, ""));
+
+        // skip duplicated by accident
+        if (commandCache.has(cmdName)) {
+          console.warn(`/${cmdName} command is found duplicated.`);
+          continue;
+        };
+
+        const cmdDescription = command.info?.description || "Unknown.";
+        const truncatedDesc = cmdDescription.length > 100 ? truncateString(cmdDescription, 97) : cmdDescription;
+        const optionsMap = command.args?.map(x => {
+          return {
+            ...x,
+            name: x.name.toLowerCase(),
+            description: x?.description.length > 100 ? truncateString(x?.description, 97) : x?.description
+          };
+        });
+
+        finaleCommand.push({
+          name: cmdName,
+          description: truncatedDesc,
+          type: Constants.ApplicationCommandTypes.CHAT_INPUT,
+          options: optionsMap || []
+        });
+
+        commandCache.set(cmdName, {
+          path: commandPath,
+          raw: cmdName
+        });
+
+        continue;
+      };
+    };
+
+    await client.rest.applicationCommands.bulkEditGuildCommands(client.user.id, process.env.PERSONALIZED_GUILD_ID!, finaleCommand);
+
+    // log, its not neccesary
+    return console.log(`Generated slash commands for ${Math.round(performance.now() - firstTime).toLocaleString()} ms.`);
+  } catch (error) {
+    console.error(error);
+    return process.exit(1);
+  };
+};
